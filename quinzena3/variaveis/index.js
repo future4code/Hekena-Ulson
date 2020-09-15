69647d7dac460192afc998de38fb26d5a433c225
helenarangel <helenarangel001@gmail.com>
2020-09-15 14:30:18 -0300

    exercício quinzena3-aula9

---
+++ quinzena3/variaveis/index.js
@@ -0,0 +1,69 @@
+/* Exercício 1- Interpretação
+        10 
+        10, 5
+*/
+
+/* Exercício 2 - Interpretação
+        10, 10, 10 
+*/
+
+//Exercicio 1 - Escrita
+// a) let nome 
+// b) let idade
+// c) console.log(typeof(nome, idade))
+// d) Apareceu no console que o type é undefined, porque não atribui nada às variáveis e por isso o programa não pode identificá-las.
+
+// d) 
+/*      const nome = prompt("Qual é o seu nome?")
+        const idade = prompt("Qual é sua idade?")
+        console.log(typeof(nome, idade)) */
+// e) Notei que as variáveis são consideradas string.
+// f)   console.log("Olá", nome, "você tem", idade, "anos")
+
+
+/* Exercicio 2 - Escrita
+        const rua = prompt("Qual a Rua em que você mora?")
+        const numero= prompt("Qual o Numero da sua residencia?")
+        const calopsita = prompt("Você gosta calopsitas?")
+        const decisao = prompt("Você casaria com o Faustão ou com o Silvio Santos?")
+        const trabalho = prompt("Com o que você trabalha?")
+
+        console.log("1. Qual a rua em que você mora?")
+        console.log(rua)
+        console.log("2. Qual o Numero da sua residencia?")
+        console.log(numero)
+        console.log("3. Você gosta calopsitas?")
+        console.log(calopsita)
+        console.log("4. Você casaria com o Faustão ou com o Silvio Santos?")
+        console.log(decisao)
+        console.log("5. Com o que você trabalha?")
+        console.log(trabalho) */
+
+// Exercicio 3 - Escrita 
+/* a)   const comidas = ['brocolis', 'arroz', 'feijao', 'tapioca', 'bananinha']
+        console.log(comidas) */
+/* b)   const comidas = ['brocolis', 'arroz', 'feijao', 'tapioca', 'bananinha']
+        console.log("Essas são minhas comidas preferidas")
+        console.log(comidas[0])
+        console.log(comidas[1])
+        console.log(comidas[2])
+        console.log(comidas[3])
+        console.log(comidas[4]) */
+
+/* c)   const usuario = prompt("Qual sua comida preferida?")
+        const comidas = ['brocolis', usuario, 'feijao', 'tapioca', 'bananinha']
+        console.log(comidas[0])
+        console.log(comidas[1])
+        console.log(comidas[2])
+        console.log(comidas[3])
+        console.log(comidas[4]) */
+
+// Exercicio 4 - Escrita 
+
+/*      const azul = true 
+        const vermelho = false
+        const amarelo = true 
+        const roupas = [azul, vermelho, amarelo]
+        console.log("Você está usando uma camiseta azul?", roupas[0])
+        console.log("Você está usando uma calça vermelha?", roupas[1])
+        console.log("Você tem um vestido amarelo?", roupas[2]) */
