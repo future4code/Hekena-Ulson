7cb3bc0031be93e0cd2f760a287508ee5af9e8bd
helenarangel <helenarangel001@gmail.com>
2020-10-31 19:41:51 -0300

    Aula 21, Quinzena6

---
+++ quinzena6/LabenuForms/labenu-forms/src/Components/Final.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class Final extends React.Component {
+  render() {
+    
+
+  return (
+    <div>
+      <h1>O FORMULÁRIO ACABOU</h1>
+      <h3>Muito obrigada por participar! Entraremos em contato! </h3>
+    </div>
+  )
+}
+}
+
